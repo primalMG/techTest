c305461a675480f0068ab71111b605aa630d7732
primalMG <danielmarcus05@hotmail.com>
2018-11-22 16:20:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      filteredPosts: '',
+      posts: [{
+        name: '',
+        discussion_url: '',
+        tagline: '',
+        user: [{
+          user: '',
+        }],
+        screenshot_url: [{
+          '300px' : ''
+        }]
+      }],
+    }
+  }
+
+  filterProduct = () => {
+    let posts = this.state.posts;
+
+    posts = posts.filter((post) => {
+      let postName = post.name.toLocaleLowerCase()
+      return postName.indexOf(
+        postName.toLocaleLowerCase()) !== -1
+    })
+
+    this.setState({ posts })
+  }
+
+
+
+  componentDidMount() {
+    fetch('https://api.producthunt.com/v1/posts',{
+      method: 'Get',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 1235a98f2491b08eeebdc2a992d9286399099c38f32449774f3ab70ebd17d418 ',
+        'Host': 'api.producthunt.com',
+      },
+    }).then(results => { return results.json()
+    }).then(post => {
+      this.setState({ posts: post.posts })
+      console.log(this.state.posts)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  renderPosts(){  
+    return <div className="content">
+      {this.state.posts.map((p, i) => 
+        <ul>
+          <li>
+            <h4>Author: {p.user.name}</h4>
+            <h3><a href={p.discussion_url}>{p.name}</a></h3>
+            <p>{p.tagline}</p>
+          </li>
+        </ul> 
+      )}   
+    </div>;   
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+          <input type="text" onChange={this.filterProduct.bind(this)} value={this.state.filteredPosts} placeholder="Search here..."/>
+
+         {this.renderPosts()}
+
       </div>
+        
+        
     );
   }
 }
