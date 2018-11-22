a7edd33733d32f7f51eb2efbf68215f07ccca71c
primalMG <danielmarcus05@hotmail.com>
2018-11-22 16:58:17 +0000

    sorting minor things

--- src/App.js
+++ src/App.js
@@ -22,17 +22,13 @@ class App extends Component {
 
   filterProduct = () => {
     let posts = this.state.posts;
-
     posts = posts.filter((post) => {
       let postName = post.name.toLocaleLowerCase()
       return postName.indexOf(
         postName.toLocaleLowerCase()) !== -1
     })
-
     this.setState({ posts })
   }
-
-
 
   componentDidMount() {
     fetch('https://api.producthunt.com/v1/posts',{
@@ -59,8 +55,9 @@ class App extends Component {
         <ul>
           <li>
             <h4>Author: {p.user.name}</h4>
-            <h3><a href={p.discussion_url}>{p.name}</a></h3>
+            <h3><a href={p.discussion_url} target="_blank" rel="noopener noreferrer">{p.name}</a></h3>
             <p>{p.tagline}</p>
+            <h5>Votes: {p.votes_count}, Comments: {p.comments_count}</h5>
           </li>
         </ul> 
       )}   
@@ -70,14 +67,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-          <input type="text" onChange={this.filterProduct.bind(this)} value={this.state.filteredPosts} placeholder="Search here..."/>
-
+      <header>
+        RideTo Tech Test
+      </header>
+        <input type="text" onChange={this.filterProduct.bind(this)} value={this.state.filteredPosts} placeholder="Search here..."/>
          {this.renderPosts()}
-
       </div>
-        
-        
     );
   }
 }
